37fdd26e86344bf585660ea3129d2ba138290f2c
Florian Klemt <FKlemt@gmx.de>
2018-09-21 21:09:39 +0200

    Initial Commit

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level : MonoBehaviour {
+
+    // Use this for initialization
+    public Transform black_hole;
+    void Start()
+    {
+        MapGenerator map_generator = new MapGenerator(50,50,0.01f,0.2f);
+        int[,] map = map_generator.generate_map();
+        for(int i=0; i<map_generator.width; i++)
+        {
+            for (int j = 0; j < map_generator.height; j++)
+            {
+                if (map[i, j] == 1)
+                {
+                    Instantiate(black_hole, new Vector3(i, 0, j), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MapGenerator
+{
+    public int width, height;
+    public float max_distance, min_blackhole_prob, max_blackhole_prob, blackhole_prob_range;
+    System.Random random;
+    public MapGenerator(int width, int height, float min_blackhole_prob, float max_blackhole_prob)
+    {
+        this.width = width;
+        this.height = height;
+        this.max_distance = new Vector2(width, height).magnitude;
+        this.min_blackhole_prob = min_blackhole_prob;
+        this.max_blackhole_prob = max_blackhole_prob;
+        this.blackhole_prob_range = max_blackhole_prob - min_blackhole_prob;
+        this.random = new System.Random();
+    }
+
+    public int[,] generate_map()
+    {
+        //TODO generate start and end
+        var start_pos = new Vector2(1, 1);
+        var target_pos = new Vector2(width - 1, height - 1);
+
+        int[,] map = new int[width, height];
+
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < height; j++)
+            {
+                float distance_to_goal = Vector2.Distance(new Vector2(i, j), target_pos);
+                float normalized_distance_to_goal = distance_to_goal / max_distance;
+
+                float probability = min_blackhole_prob 
+                                    + blackhole_prob_range * normalized_distance_to_goal;
+                Debug.Log(probability);
+                if(random.NextDouble() < probability){
+                    //is black hole
+                    map[i,j] = 1;
+                }
+                else
+                {
+                    map[i, j] = 0;
+                }
+            }
+        }
+        return map;
+    }
+
+}
