0d2f0016a974937bbbaf33444afabb5777d73b29
Florian Klemt <FKlemt@gmx.de>
2018-09-22 21:59:11 +0200

    Added Shield powerup and shield logic

---
+++ Assets/Scripts/ShieldController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShieldController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.tag == "Astroid")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
