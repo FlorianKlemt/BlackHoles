503ac148424c0c2d4218bbc14fd7683e1effe758
Florian Klemt <FKlemt@gmx.de>
2018-09-22 19:16:57 +0200

    HUD

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,27 +9,42 @@ public class PlayerController : MonoBehaviour {
     public Rigidbody player_rb;
     public bool alive;
 
+    private PlayerFuel player_fuel;
+
     void Start()
     {
         alive = true;
         player_rb = GetComponent<Rigidbody>();
+        player_fuel = GetComponent<PlayerFuel>();
     }
 
     void FixedUpdate()
     {
         if (alive)
-        {
+        {   
             player_rb.velocity *= damping;
             if (Input.GetKey(KeyCode.A))
+            {
                 player_rb.AddForce(Vector3.left * thrust);
+                player_fuel.useFuel(1);
+            }
             //player_rb.transform.Rotate(-Vector3.up * rotate_speed * Time.fixedDeltaTime);
             if (Input.GetKey(KeyCode.D))
+            {
                 player_rb.AddForce(Vector3.right * thrust);
+                player_fuel.useFuel(1);
+            }
             //player_rb.transform.Rotate(Vector3.up * rotate_speed * Time.fixedDeltaTime);
             if (Input.GetKey(KeyCode.W))
+            {
                 player_rb.AddForce(Vector3.forward * thrust);
+                player_fuel.useFuel(1);
+            }
             if (Input.GetKey(KeyCode.S))
+            {
                 player_rb.AddForce(Vector3.back * thrust);
+                player_fuel.useFuel(1);
+            }
         }
         if (player_rb.velocity != Vector3.zero){
             transform.rotation = Quaternion.LookRotation(player_rb.velocity, Vector3.up);
